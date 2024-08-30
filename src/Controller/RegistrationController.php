a92f8433f4d457828569660213ab3a028292b2cf
Ariel Lizarazu <alizarazu@gmail.com>
2024-08-29 22:48:01 -0400

    Update login form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,7 +37,7 @@ class RegistrationController extends Controller
             // ... do any other work - like sending them an email, etc
             // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('/');
+            return $this->redirectToRoute('/register');
         }
 
         return $this->render(
