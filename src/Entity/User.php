5d08a12fb3d6ff40e9dd6d426747c0651a0e4f02
Ariel Lizarazu <alizarazu@gmail.com>
2024-08-29 21:10:05 -0400

    Update Register controller

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,113 +1,119 @@
 <?php
 
+// src/AppBundle/Entity/User.php
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
+ * @ORM\Entity
+ * @UniqueEntity(fields="email", message="Email already taken")
+ * @UniqueEntity(fields="username", message="Username already taken")
  */
 class User implements UserInterface
 {
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank
+     * @Assert\Email
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank
      */
     private $username;
 
     /**
-     * @ORM\Column(type="json")
+     * @Assert\NotBlank
+     * @Assert\Length(max=4096)
      */
-    private $roles = [];
+    private $plainPassword;
 
     /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
+     * The below length depends on the "algorithm" you use for encoding
+     * the password, but this works well with bcrypt.
+     *
+     * @ORM\Column(type="string", length=64)
      */
     private $password;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
     /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
+     * @ORM\Column(type="array")
      */
-    public function getUsername(): string
+    private $roles;
+
+    public function __construct()
     {
-        return (string) $this->username;
+        $this->roles = array('ROLE_USER');
     }
 
-    public function setUsername(string $username): self
+    // other properties and methods
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function setUsername($username)
     {
         $this->username = $username;
-
-        return $this;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
+    public function getPlainPassword()
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+        return $this->plainPassword;
     }
 
-    public function setRoles(array $roles): self
+    public function setPlainPassword($password)
     {
-        $this->roles = $roles;
-
-        return $this;
+        $this->plainPassword = $password;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function getPassword(): string
+    public function getPassword()
     {
         return $this->password;
     }
 
-    public function setPassword(string $password): self
+    public function setPassword($password)
     {
         $this->password = $password;
-
-        return $this;
     }
 
-    /**
-     * Returning a salt is only needed, if you are not using a modern
-     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
-     *
-     * @see UserInterface
-     */
-    public function getSalt(): ?string
+    public function getSalt()
     {
+        // The bcrypt algorithm doesn't require a separate salt.
+        // You *may* need a real salt if you choose a different encoder.
         return null;
     }
 
-    /**
-     * @see UserInterface
-     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
     public function eraseCredentials()
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 }
